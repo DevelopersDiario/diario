ec5bdd9f4b559f44defafee415bfce0c9d94072f
benya <benjaminlopezluis@gmail.com>
2018-11-08 03:13:10 +0000

    commit final

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+<br>
+<br>
+<br>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -8,31 +12,31 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+                            <label for="correo" class="col-sm-4 col-form-label text-md-right">{{ __('Correo electronico') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="correo" type="correo" class="form-control{{ $errors->has('correo') ? ' is-invalid' : '' }}" name="correo" value="{{ old('correo') }}" required autofocus>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                @if ($errors->has('correo'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('correo') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
@@ -41,9 +45,25 @@
 
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
+                               <input type="image" id="image" alt="Login"    src="images/iconos/fb.png" width="100%" />
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                               <input type="image" id="image" alt="Login"    src="images/iconos/goo.png" width="100%" />
+                            </div>
+                        </div>
+
+
+
+
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Recordar Contraseña') }}
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Recuerdame') }}
                                     </label>
                                 </div>
                             </div>
@@ -56,7 +76,7 @@
                                 </button>
 
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Olvidaste tu contraseña?') }}
+                                    {{ __('¿Olvidaste tu contraseña?') }}
                                 </a>
                             </div>
                         </div>
--- resources/views/layout/inicio.blade.php
+++ resources/views/layout/inicio.blade.php
@@ -4,6 +4,8 @@
   Diario Interactivo
 @endsection
 
+
+
 @section('header-content')
 <div class="container-fluid navbar-dark bg-dark fixed-top">
   <nav class="navbar navbar-expand-lg  container ">
@@ -17,13 +19,13 @@
     <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
       <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
         <li class="nav-item active ml-2">
-          <a class="nav-link" href="#inicio" data-click="scroll-to-target">INICIO</a>
+          <a class="nav-link" href="#inicio">INICIO</a>
         </li>
         <li class="nav-item ml-2">
-          <a class="nav-link" href="#nosotros" data-click="scroll-to-target">NOSOTROS</a>
+          <a class="nav-link" href="#nosotros">NOSOTROS</a>
         </li>
         <li class="nav-item ml-2">
-          <a class="nav-link" href="#contacto"  data-click="scroll-to-target">CONTACTO</a>
+          <a class="nav-link" href="#contacto" >CONTACTO</a>
         </li>
         <li class="nav-item ml-2 py-1">
           <a href="#" onclick="window.location='{{ url("register") }}'"  data-toggle="modal" data-target="#registros" class="btn btn-outline-primary btn-block ">REGISTRARME</a>
@@ -40,18 +42,138 @@
 @endsection
 
 @section('main-content')
-<section id="inicio">
-  <div class="container-fluid">
-    <div class="row" >
-      <div   class="col-lg-10" class="col-md-8" class="col-sm-10"  class="col-xl-12">
-        <img class="imagen" src="images/diario.png" class="img-responsive" width="500" height="580">
-        <h1><p class="inicio">CAPTURA TUS</p></h1>
-        <h2><p class="momentos">MOMENTOS</p></h2>
-        <h3><p>Guarda tus archivos de las actividades</p></h3>
-        <h3><p>escolares en tu diario personal. </p></h3>
-     </div>
+<section id="main-tmpl">
+        <div class="item " >
+<section class="mainx-tmpl">
+  <div class="item">
+    <div id="inicio">
+      <div class="container-fluid">
+        <div class="row" >
+          <div   class="col-lg-10" class="col-md-8" class="col-sm-10"  class="col-xl-12">
+            <img class="imagen" src="images/diario.png" class="img-responsive" width="500" height="580">
+            <h1><p class="inicio">CAPTURA TUS</p></h1>
+            <h2><p class="momentos">MOMENTOS</p></h2>
+            <h3><p>Guarda tus archivos de las actividades</p></h3>
+            <h3><p>escolares en tu diario personal. </p></h3>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-</div>
 </section>
+
+       </div>
+   </section>
+
 @endsection
+
+@section('nosotros')
+<section id="main-tmpl">
+        <div class="item " id="nosotros">
+
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-xl-8" >
+          <h2  class="text-center" id="obj"><strong>Objetivo</strong> <i class="fa fa-map-marker pull-right hidden-xs-down" aria-hidden="true"></i></h2>
+          <p class="text-justify ml-5 mr-5">
+              Objetivo Comprender los procesos inherentes al pensamiento reflexivo que se generan a través del diario interactivo en línea, realizado por un  grupo estudiantes en sus escenarios reales de práctica. <br clear="none">DidacTIC imgObject Diseño didáctico Interactivo (DidacTIC) es una asociación civil que nació con el propósito de crear diseños tecnopedagógico que faciliten a alumnos y docentes la enseñanza y el aprendizaje reflexivo. <br clear="none"> Realiza diagnósticos sobre el uso de las tecnologías de la información en el ámbito educativo en sus diferentes niveles de acuerdo a su entorno y contexto. DidacTIC está consciente de la presencia de la tecnología en las actividades diarias y sobre todo, las tecnologías móviles, por esto busca captar la atención y consigo el interés del alumno y docentes en formación y servicio desarrollando competencias en el uso de las TIC mediante herramientas tecnológicas que apoyen la práctica docente como la aplicación móvil del Diario Interactivo, los e-portafolios, donde el estudiante capture sus momentos de aprendizaje y obtenga a través de la reflexión de los escenarios un aprendizaje constructivista. DidacTIC impulsa el uso de las TIC con el afán de ser partícipe del desarrollo educativo y tecnológico en Oaxaca.
+          </p>
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4" >
+        <img src="images/didactico.png" style="position: relative; width: 75%; height: auto; left: 12%;" />
+      </div>
+    </div>
+
+       </div>
+   </section>
+@endsection
+
+
+@section('contacto')
+<section id="main-tmpl">
+        <div class="item " id="contacto">
+
+
+      <div class="container-fluid">
+        <div class="row" >
+          <div   class="col-lg-10" class="col-md-8" class="col-sm-10"  class="col-xl-12">
+            <img src="images/mochis.png"  width="300" height="400" class="img-responsive" align="left" >
+            <br>
+            <br>
+            <h3 class="redes"><p>
+                <a class="txtwhi" target="_blank" href="https://www.facebook.com/didactic/"> <img src="images/sobre.svg" width="40" height="40" > didaticsoax@gmail.com  <br clear="none"></a></p></h3>
+                <br>
+                <br>
+                 <h3><p><a class="txtwhi" target="_blank" href="https://www.gmail.com/didactic/"> <img src="images/facebook.svg" width="40" height="40" > didaticsoax@gmail.com</a></p></h3>
+         </div>
+      </div>
+    </div>
+ </div>
+   </section>
+
+
+
+
+
+
+@endsection
+
+@section('footer')
+  <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12" >
+<center>
+            <p class="txtwhi"> <strong class="txtwhi">didaticsoax@gmail.com </strong>   &nbsp;&copy;2018- Todos los derechos reservados. Diseñado por <a class="txtwhi" href="index.php">DidacTIC--Diseño Didactico Interactivo AC</a></p>
+    </center>
+          </div>
+
+          <div class="col-sm-12 text-xs-right" style="padding-top: 2%;"><center>
+            <ul class="list-inline">
+
+              <li class="list-inline-item">
+                <a class="txtwhi"  href="index.php">Inicio</a>
+              </li>
+              <li class="list-inline-item">
+                <a class="txtwhi" href="aviso.html">Aviso legal</a>
+              </li>
+              <li class="list-inline-item">
+                <a class="txtwhi" href="#" data-toggle="modal" data-target="#miMod666">Terminos y condiciones</a>
+              </li>
+            </ul></center>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="modal fade" id="miMod666" tabindex="-1" role="dialog" arialabelledby="myModalLabel" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                     <h5 class="modal-title">Términos y condiciones</h5>
+                    <button type="button" class="close" data-dismiss="modal" arialabel="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                <div class="contenedor">
+                <div class="Terminos">
+                    <h4> <p> He leído y acepto los términos de la aplicación.
+      Es responsable del uso de los servicios por cualquier contenido que publique y de las consecuencias derivadas. El contenido que envíe, publique o muestre podrá ser visto por otros usuarios de los servicios a través de servicios y sitios web de terceros. Solo deberá proporcionar el contenido que quiera compartir con otras personas en estos Término. Tip: Lo que digas en Mi Diario se verá alrededor del mundo instantáneamente. ¡Eres lo que publicas! El usuario únicamente podrá hacer uso de estos servicios si acepta un contrato vinculante con Mi Diario como también a recibir dichos servicios según las leyes de México u otra jurisdicción aplicable. El usuario solo podrá hacer uso de dicho Servicios conforme a dichas condiciones como a toda ley, normativa o reglamento aplicable sea local, estatal, nacional o internacional.
+      </p></h4>
+           </div>
+           </div>
+
+           </div>
+          </div>
+        </div>
+        </div>
+
+    </footer>
+@endsection
+
+
 @yield('main-content')
+@yield('nosotros')
+@yield('contacto')
+@yield('footer')
